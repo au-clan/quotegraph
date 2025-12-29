1e27d9405470441cf56876812b7759e5cff2526c
maculjak <mculyak@gmail.com>
2025-12-29 12:26:46 +0000

    Add script that converts Wikidata JSON dump to Parquet

---
+++ scripts/wikidata2parquet.py
@@ -0,0 +1,15 @@
+import argparse
+import quotegraph.utils as u
+import pyspark.sql.functions as F
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('wikidata_file', type=str, required=True)
+    parser.add_argument('output_file', type=str, required=True)
+    parser.add_argument('--n_threads', type=int, default=50)
+    args = parser.parse_args()
+
+    spark = u.start_spark()
+
+    df = spark.read.json(args.wikidata_file)
+    df.write.parquet(args.output_file)
