7a4d2f3d4d9ec069dc1242d79f3101cf09c5dfd2
Marko Culjak <mculyak@gmail.com>
2025-12-31 23:50:51 +0100

    Add statement, description, and sitelink handling

--- quotegraph/utils.py
+++ quotegraph/utils.py
@@ -63,8 +63,50 @@ def get_alias_list(aliases: str | None) -> list:
 
 
 @F.udf(T.StringType())
-def get_label(labels: str|None) -> str:
+def get_label(labels: str | None) -> str:
     if labels is None:
         return ""
 
     return json.loads(labels)['value']
+
+item_statement_struct = T.StructType([
+    T.StructField('property', T.StringType(), False),
+    T.StructField('qid', T.StringType(), False)
+])
+
+@F.udf(T.ArrayType(item_statement_struct))
+def get_item_statements(line: str | None) -> list:
+    if line is None:
+        return []
+
+    claims = json.loads(line)
+    statements = []
+    
+    for prop, snaks in claims.items():
+        for snak in snaks:
+            snak = snak['mainsnak']
+            if 'datavalue' not in snak:
+                continue
+            datavalue = snak['datavalue']
+            
+            valuetype = datavalue['type']
+            if valuetype == 'wikibase-entityid' and datavalue['value']['entity-type'] == 'item':
+                if 'id' not in datavalue['value']:
+                    datavalue['value']['id'] = 'Q' + str(datavalue['value']['numeric-id'])
+                statements.append((prop, datavalue['value']['id']))
+    return statements
+
+
+
+sitelink_struct = T.StructType([
+    T.StructField("type", T.StringType(), False),
+    T.StructField("title", T.StringType(), False)
+])
+
+@F.udf(T.ArrayType(sitelink_struct))
+def get_titles(line: str | None) -> list:
+    if line is None:
+        return []
+
+    sitelinks = json.loads(line)
+    return [(i, j['title']) for i, j in sitelinks.items()]
